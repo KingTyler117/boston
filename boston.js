399bb13d28498a730bb197e8d44968c718601160
Tyler King <tyman.king97@gmail.com>
2020-04-16 12:23:36 -0700

    boston change

--- boston.js
+++ boston.js
@@ -1,11 +1,21 @@
 // prompt('Hi');
 
-
-var bestTown = prompt("what is the best town in America?");
-if (bestTown == "boston") {
-    alert("You are correct."); 
-} else {
-    alert("Wrong");
+function bestTown() {
+    var bestTown = prompt("what is the best town in America?");
+    if (bestTown == "boston") 
+    {
+        alert("You are correct."); 
+    } 
+    else 
+    {
+        alert("Wrong");
+    }
 }
-var today = new Date();
-document.write(today);
+
+function date() {
+    var today = new Date();
+    document.write(today);
+}
+
+bestTown();
+date();
