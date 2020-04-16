52f558990117735b55a6ca7f8bb6c5b7f9af9bf7
Tyler King <tyman.king97@gmail.com>
2020-04-15 21:43:45 -0700

    added java scpirt file

--- boston.js
+++ boston.js
@@ -1,4 +1,11 @@
-prompt('hi');
-var today = new Date ();
-var hourNow = today.getHours();
+// prompt('Hi');
 
+
+var bestTown = prompt("what is the best town in America?");
+if (bestTown == "boston") {
+    alert("You are correct."); 
+} else {
+    alert("Wrong");
+}
+var today = new Date();
+document.write(today);
